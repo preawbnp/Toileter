87028a7fc13001fddfa35a0f3dfa1e3c31e34953
preawbnp <preawbnp@preawbnps-MacBook-Pro.local>
2018-04-22 01:50:12 +0700

    Added Authentication function

---
+++ consts/firebase_info.js
@@ -0,0 +1,31 @@
+export const config = {
+  apiKey: "AIzaSyDBCO5q5-WHGk50mN6EzWWefGmM8gs6BW4",
+  authDomain: "toileter-f7da4.firebaseapp.com",
+  databaseURL: "https://toileter-f7da4.firebaseio.com",
+  projectId: "toileter-f7da4",
+  storageBucket: "toileter-f7da4.appspot.com",
+  messagingSenderId: "240757099437"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const config = {
+//   apiKey: "AIzaSyDaY9hbGQOr41ljQeZgX26NTd_SqKwH4oQ",
+//   authDomain: "studentloan-7fbce.firebaseapp.com",
+//   databaseURL: "https://studentloan-7fbce.firebaseio.com",
+//   projectId: "studentloan-7fbce",
+//   storageBucket: "studentloan-7fbce.appspot.com",
+//   messagingSenderId: "1016135551882"
+// };
